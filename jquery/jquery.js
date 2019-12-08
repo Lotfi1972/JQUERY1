b6ec8e719ce4c1c27f49c703d6549e426f478153
Lotfi1972 <56515116+Lotfi1972@users.noreply.github.com>
2019-12-08 14:29:38 +0100

    Add files via upload

---
+++ jquery/jquery.js
@@ -0,0 +1,35 @@
+
+$("#img1").mouseenter( function(){
+    $('#bt').show();
+    $(this).css('opacity','0.5');
+    
+})
+$("#img1").mouseleave( function(){
+    $('#bt').hide();
+    $(this).css('opacity','1');
+    
+})
+
+$("#img2").mouseenter( function(){
+    $('#bt2').show();
+    $(this).css('opacity','0.5');
+    
+})
+$("#img2").mouseleave( function(){
+    $('#bt2').hide();
+    $(this).css('opacity','1');
+    
+})
+$("#img3").mouseenter( function(){
+    $('#bt3').show();
+    $(this).css('opacity','0.5');
+    
+})
+$("#img3").mouseleave( function(){
+    $('#bt3').hide();
+    $(this).css('opacity','1');
+    
+})
+
+
+
